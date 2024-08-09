337af72efd22c8f20a5a099cd7d72e7dd0b762de
muhammadhuzaifa0987 <164562502+muhammadhuzaifa0987@users.noreply.github.com>
2024-08-09 23:30:41 +0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,29 @@
+
+
+  window.addEventListener("load", () => {
+    if (localStorage.getItem("user")) {
+      window.location.replace("./index.html");
+    }
+  });
+  
+  
+import { auth , signInWithEmailAndPassword} from "./firebase.js"
+const logInHandler = async ()=>{
+    try {
+        const email = document.querySelector("#email")
+        const password = document.querySelector("#password")
+        const responce = await signInWithEmailAndPassword(
+            auth,
+            email.value,
+            password.value
+        )
+        console.log("responce" , responce.user.uid);
+        localStorage.setItem("user",responce.user.uid)
+        window.location.href = "./index.html"
+    } catch (error) {
+        console.log("error", error.message);
+    }
+}
+
+
+window.logInHandler = logInHandler;
---
+++ signup.js
@@ -0,0 +1,39 @@
+
+import { auth ,createUserWithEmailAndPassword ,db ,doc,setDoc, signInWithEmailAndPassword} from "./firebase.js"
+
+
+const signUpHandler = async()=>{
+  console.log("WElcome")
+  try {
+      const fullName = document.querySelector ("#fullName")
+      const email = document.querySelector ("#email")
+      const password = document.querySelector ("#password")
+      const gender = document.getElementsByName ("gender")
+      const userObj = {
+          fullname : fullName.value,
+          email : email.value,
+      }
+
+      for(let g of gender){
+          if(g.checked){
+              userObj.gender = g.value
+          }
+      }
+      console.log("userObj",userObj);
+      const responce = await createUserWithEmailAndPassword(
+          auth,
+          email.value,
+          password.value
+      );
+      const uid = responce.user.uid;
+      const userResponce = await setDoc(doc(db , "user" , uid),userObj)
+      alert("user Succesfull sign in ")
+      window.location.href = "login.html"
+      
+  } catch (error) {
+      console.log("error", error.message);
+      alert(error.message);
+  }
+}
+
+window.signUpHandler = signUpHandler;
